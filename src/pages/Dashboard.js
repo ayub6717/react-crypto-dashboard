4b27f21fd65e97cffc7b5538e95ea8d3df9faa4c
Ayub6717 <ayub15-6717@diu.edu.bd>
2023-09-03 00:25:01 +0600

    [refactor] Currency block refactor

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -13,12 +13,34 @@ function Dashboard() {
                   <img className='object-cover w-full max-h-[43px]' src={IconWalet} alt="img" />
                 </div>
                 <div className="flex flex-col">
-                  <p className='text-[#000] text-[16px] leading-[28px] font-semibold word-break'>Estimated Balance</p>
+                  <p className='text-[#000] text-[16px] leading-[28px] font-semibold word-break mt-2'>Estimated Balance</p>
+                </div>
+              </div>
+              <div className='relative'>
+                <img className='w-6 h-6 mt-[10px] mr-[3px]' src={EyeIcon} alt="img" />
+              </div>
+            </div>
+            <div className='mt-[29px]'>
+               <p className='text-[#000] text-[30px] leading-[28px] font-semibold word-break'>$23,738</p>
+            </div>
+            <div className='flex justify-between items-center mt-4'>
+              <p className='text-[#626D7D] text-[13px] leading-[22px] font-medium word-break'>Monthly Profit</p>
+              <p className='text-[#219653] text-[13px] leading-[22px] font-semibold word-break'>+$2560.78</p>
+              <span className='bg-[#2196543c] rounded-[60px] px-2 text-[#219653] text-[11px] leading-[22px] font-semibold word-break'>+14.67%</span>
+            </div>
+          </div>
+          <div className='rounded-xl px-[20px] py-[22px] bg-[#FFF]'>
+            <div className='flex justify-between'>
+              <div className='flex gap-4'>
+                <div className='w-[44px] h-[33px]'>
+                  <img className='object-cover w-full max-h-[43px]' src={IconWalet} alt="img" />
+                </div>
+                <div className="flex flex-col">
+                  <p className='text-[#000] text-[16px] leading-[28px] font-semibold word-break'>BTCUSDT</p>
                   <p className='text-[#626D7D] text-[13px] leading-[22px] font-medium word-break'>Bitcoin</p>
                 </div>
               </div>
               <div className='relative'>
-                <img className='w-6 h-6' src={EyeIcon} alt="img" />
                 <div className='absolute right-2 top-[28px] w-[133px] h-[56px]'>
                     <img className='object-cover w-full max-h-[56px]' src={VectorIcon } alt="img" />
                 </div>
@@ -28,11 +50,12 @@ function Dashboard() {
                <p className='text-[#000] text-[20px] leading-[22px] font-semibold word-break'>$23,738</p>
             </div>
             <div className='flex justify-between items-center mt-[19px]'>
-              <p className='text-[#626D7D] text-[13px] leading-[22px] font-medium word-break'>Monthly Profit</p>
+              <p className='text-[#626D7D] text-[13px] leading-[22px] font-medium word-break'>PNL Daily</p>
               <p className='text-[#219653] text-[13px] leading-[22px] font-semibold word-break'>+$2560.78</p>
               <span className='bg-[#2196543c] rounded-[60px] px-2 text-[#219653] text-[11px] leading-[22px] font-semibold word-break'>+14.67%</span>
             </div>
           </div>
+          
         </div>
     </div>
   )
