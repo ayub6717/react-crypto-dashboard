7d2c69885c80b2234293f29230327e475686a13e
Ayub6717 <ayub15-6717@diu.edu.bd>
2023-09-03 23:36:16 +0600

    [feat] Generate table data

---
+++ src/data/currencyData.js
@@ -0,0 +1,74 @@
+// customerData.js
+import UpIcon from '../assets/svg/UpIcon.svg'
+import DownIcon from '../assets/svg/DownIcon.svg'
+export const currentRateDate = [
+    {
+      currency: 'BTCUSDT',
+      digital_currency: 'Bitcoin ',
+      amount: '$23,495',
+      commission: '+23.4%',
+    },
+    {
+      currency: 'AXSUSDT',
+      digital_currency: 'Axie Infinity',
+      amount: '$15.9',
+      commission: '-7.8%',
+    },
+    {
+      currency: 'ETHUSDT',
+      digital_currency: 'Ethereum',
+      amount: '$15,978',
+      commission: '-0.3%',
+    },
+    {
+      currency: 'SOLUSDT',
+      digital_currency: 'Solana',
+      amount: '$495',
+      commission: '+11.1%',
+    },
+    {
+      currency: 'BNBUSDT',
+      digital_currency: 'Binance',
+      amount: '$267',
+      commission: '+6.7%',
+    },
+    {
+      currency: 'ADAUSDT',
+      digital_currency: 'Cardano',
+      amount: '$0.49',
+      commission: '-1.4%',
+    },
+  ];
+  
+  export const rateHistoryData = [
+    {
+      img: DownIcon,
+      to_currency: 'Deposit ',
+      to_amount: '+$23,738',
+      time: '11:34 PM',
+      badge: 'Pending',
+    },
+    {
+      img: UpIcon,
+      to_currency: 'SANDUSDT',
+      to_amount: '-$576',
+      time: '06:01 AM',
+      badge: 'Done',
+    },
+    {
+      img: DownIcon,
+      to_currency: 'DOGEUSDT',
+      to_amount: '+$3500',
+      time: '02:10 AM',
+      badge: 'Rejected',
+    },
+    {
+      img: UpIcon,
+      to_currency: 'FTMUSDT',
+      to_amount: '-$791',
+      time: '06:45 PM',
+      badge: 'Rejected',
+    },
+  ];
+  
+  
