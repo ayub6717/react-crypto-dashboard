126a80df30938009f6f59ccbc3ba2c8e7eb3a461
Ayub6717 <ayub15-6717@diu.edu.bd>
2023-09-04 19:11:17 +0600

    [feat] Create reusable button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <>
+        <button className='text-[#8c8c8c] text-[10px] font-semibold leading-[22px] border border-[#E5E5E5] dark:border-[#303036] dark:bg-[#24242B] rounded-[7px] px-2.5 py-0.5 inline-block'>View All</button>
+    </>
+  )
+}
+
+export default Button
